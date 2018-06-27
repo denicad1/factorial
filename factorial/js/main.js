656a20c61affc675c0ed3d8cffe4692346147294
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-06-27 14:25:04 -0400

    factorial program

--- factorial/js/main.js
+++ factorial/js/main.js
@@ -1,1 +1,11 @@
-﻿// Your code here!
+﻿function factorial(num) {
+    //holds the value of the factorial
+    var factorialProduct = 1;
+    //for loop to get the factorial
+    for (var i = num; i >= 1; i--) {
+        factorialProduct *=i ;
+    };
+    //returns the value of the factorial product to be logged on the console
+    return factorialProduct;
+};
+console.log(factorial(4));
